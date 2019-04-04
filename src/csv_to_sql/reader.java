2330229c46a52a5f1c3e9a5bab762a790ffad1d8
openbravo <openbravo@openbravo-HP-Pavilion-g6-Notebook-PC>
2019-04-04 12:14:00 +0200

    Es fea de collons i molt optimitzable, pero funciona i es eficient

--- src/csv_to_sql/reader.java
+++ src/csv_to_sql/reader.java
@@ -1,8 +1,10 @@
 package csv_to_sql;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
@@ -30,7 +32,7 @@ public class reader {
 		 */
 		
 		
-		insert("tabla");
+		delete("tabla");
 		
 		/*
 		 * 3 Funciones principales con nombres autodescriptivos
@@ -43,8 +45,8 @@ public class reader {
 	}
 	
 	public static void insert(String tablename) throws IOException {
-		String filename = "insert_"+tablename;
-		String query = "INSERT into "+tablename+"(";
+		String filename = "insert_"+tablename+".sql";
+		String query = "INSERT into "+tablename+" (";
 		try {
 			Reader reader = Files.newBufferedReader(Paths.get("/home/openbravo/Escritorio/ejemplo.csv"));
 			CSVReader csvreader = new CSVReader(reader);
@@ -71,27 +73,73 @@ public class reader {
 						continue;
 					}
 					else {
-						consulta+= query+" values ";
+						consulta+= query+" values (";
 						int counter =0;
 						for(String s2: splited) {
 							if(counter!=splited.length-1) {
 								consulta +=  "'"+s2+"', ";
 								counter++;
 							}
-							else consulta+= s2+";\n";
+							else consulta+= "'"+s2+"');\n";
 						}
 						
 					}
 					
 				}
 			}
-			System.out.println(consulta);
+			File f = new File("/home/openbravo/Escritorio/"+filename);
+			FileWriter fw = new FileWriter(f);
+			fw.write(consulta);
 			reader.close();
 			csvreader.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
+	
+	public static void delete(String tablename) throws IOException {
+		String filename = "delete_"+tablename+".sql";
+		String query = "Delete from "+tablename+" where ";
+		try {
+			Reader reader = Files.newBufferedReader(Paths.get("/home/openbravo/Escritorio/ejemplo.csv"));
+			CSVReader csvreader = new CSVReader(reader);
+			String[] array;
+			String[] campos = null;
+			String consulta ="";
+			boolean flag=false;
+			while((array = csvreader.readNext()) != null) {
+				for(String s : array) {
+					String[] splited = s.split(";");
+					if(!flag) {
+						campos = splited;
+						flag=true;
+						continue;
+					}
+					else {
+						consulta+= query;
+						int counter =0;
+						for(String s2: splited) {
+							if(counter!=splited.length-1) {
+								consulta +=  campos[counter]+" = '"+s2+"', ";
+								counter++;
+							}
+							else consulta+= campos[counter]+" = '"+s2+"';\n";
+						}
+						
+					}
+					
+				}
+			}
+			File f = new File("/home/openbravo/Escritorio/"+filename);
+			FileWriter fw = new FileWriter(f);
+			fw.write(consulta);
+			reader.close();
+			csvreader.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	
 	/*Funciona 
 	public static void test() throws IOException {
